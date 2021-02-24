d0bfb020736870c824cc0074827743a8e5df61fe
Khelil_Yasser <lmutsuha79@gmail.com>
2021-02-24 13:55:46 +0100

    add nav bar for mobile and tablet

--- js/main.js
+++ js/main.js
@@ -17,3 +17,23 @@ function add_shadow_to_nav(event) {
 
 };
 //###########################################
+let nav_button = document.getElementById("nav_button");
+let nav_bar_container = document.querySelector("#nav_bar .container");
+let nav_conter = 0;
+let nav_bar = document.getElementById('nav_bar');
+nav_button.addEventListener('click', show_mobile_nav);
+
+function show_mobile_nav() {
+    if (nav_conter == 0) {
+        nav_bar.style.height = 'auto'
+        nav_bar_container.style.visibility = 'visible';
+        nav_bar_container.style.opacity = '1';
+
+        nav_conter = 1
+    } else {
+        nav_bar.style.height = '50px'
+        nav_bar_container.style.visibility = 'hidden';
+        nav_bar_container.style.opacity = '0';
+        nav_conter = 0
+    }
+};
