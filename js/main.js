76eaa6fdf3b35852422b61f5f26de8d5868c7ebf
Khelil_Yasser <lmutsuha79@gmail.com>
2021-02-27 18:08:25 +0100

    remove bug (when resize screan when mobile nav bar open)

--- js/main.js
+++ js/main.js
@@ -11,19 +11,38 @@ function show_mobile_nav() {
     if (nav_but_conter === 0) {
         nav_bar.style.width = '50%';
         nav_but.style.right = 'calc(50% - 30px)';
-        over_lay.style.visibility = 'visible'
+        over_lay.style.visibility = 'visible';
         over_lay.style.opacity = 0.7;
 
 
         nav_but_conter = 1;
     } else {
-        nav_bar.style.width = '0%';
-        nav_but.style.right = '10px';
-        over_lay.style.visibility = 'hidden'
+        nav_but_conter = 0;
+        reset_nav_width();
+    }
+
+};
+
+
+
+let media = matchMedia("(min-width: 730px)");
+media.addEventListener('change', reset_nav_width);
+function reset_nav_width() {
+    console.log(media.matches)
+    if (media.matches == true) {
+        nav_bar.style.width = '100%';
+        console.log("if ok");
+        over_lay.style.visibility = 'hidden';
         over_lay.style.opacity = 0;
 
-        nav_but_conter = 0;
+    }
+    else {
+        nav_bar.style.width = '0%';
+        nav_but.style.right = '10px';
+        over_lay.style.visibility = 'hidden';
+        over_lay.style.opacity = 0;
+        console.log("else ok");
 
-    };
+    }
 
 }
